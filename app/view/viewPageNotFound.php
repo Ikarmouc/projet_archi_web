f026964c28219db1db82842dfb80fb92dc016213
Ismail Karmouche <ismail.karmouche@etudiant.univ-lr.fr>
2021-12-30 12:48:44 +0100

    update 404 page

--- app/view/viewPageNotFound.php
+++ app/view/viewPageNotFound.php
@@ -3,34 +3,24 @@ namespace app\view;
 include ("app/view/header.php");
 ?>
 
-    <div class="row" style="display: flex; align-content: center">
-        <h1 style="color: white">
-            Oops!
-        </h1>
-        <div>
-            <img src="./img/404.jpg" width="50%">
-        </div>
 
-
-        <div class="">
-            La page souhaitée n'existe pas ou n'existe plus
-        </div>
-        <div class="">
-            <button class="btn btn-info">
-                <a href="index.php"> Retour a l'accueil</a>
-            </button>
-        </div>
-
-        <div class="col-md-12" style="display: flex; align-content: center; justify-content: center;flex-direction: row; margin-top: 25%">
-            <div class="" style="color: white">
-
-
-
-            </div><div>
-            </div>
-        </div>
+<div style="display: flex; justify-content: center;text-align: center; flex-direction: column">
+    <h1 style="color: white">
+        Oops!
+    </h1>
+    <div style="color: white">
+        La page souhaitée n'existe pas ou n'existe plus
+    </div>
+    <div class="">
+        <button class="btn btn-info">
+            <a href="index.php"> Retour a l'accueil</a>
+        </button>
     </div>
 
+    <div>
+        <img src="./img/404.jpg" width="50%">
+    </div>
 
+</div>
 
     <?php include("app/view/footer.php")?>
